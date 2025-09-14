8a14db07b8ee0cde9afc0694d5ba1828d1230faa
Tapan Radadiya <84001495+Tapan-Radadiya@users.noreply.github.com>
2025-09-14 14:22:49 +0530

    create testcases for the go quiz service

--- Quiz_service_Go/Testcases/mcqTest/mcq_test.go
+++ Quiz_service_Go/Testcases/mcqTest/mcq_test.go
@@ -6,69 +6,117 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"os"
 	"quiz_service/model"
 	"testing"
 
 	"github.com/google/uuid"
+	"github.com/joho/godotenv"
 )
 
-var tempMCq uuid.UUID
+var tempMcqId uuid.UUID
+var tempQuizId uuid.UUID
 
-type createMcq struct {
-	Question string      `json:"question"`
-	Options  []mcqOption `json:"options"`
-}
-
-type mcqOption struct {
-	Option    string `json:"option"`
-	IsCorrect bool   `json:"isCorrect"`
-}
-
+// MCQ Related
 func TestCreateMcq(t *testing.T) {
+	godotenv.Load("../../.env")
 
-	payload := createMcq{
+	payload := model.McqData{
 		Question: "Which statement about method overloading and overriding is true?",
-		Options: []mcqOption{
+		Options: []model.McqOptions{
 			{
-				Option:    "Method overloading occurs when two methods have the same name but different parameter lists in the same class.",
-				IsCorrect: true,
+				OptionText: "Method overloading occurs when two methods have the same name but different parameter lists in the same class.",
+				IsCorrect:  true,
 			},
 			{
-				Option:    "Method overriding requires methods to have different return types.",
-				IsCorrect: false,
+				OptionText: "Method overriding requires methods to have different return types.",
+				IsCorrect:  false,
 			},
 			{
-				Option:    "Overloaded methods must be in different classes.",
-				IsCorrect: false,
+				OptionText: "Overloaded methods must be in different classes.",
+				IsCorrect:  false,
 			},
 			{
-				Option:    "Overridden methods cannot call the superclass version of the method.",
-				IsCorrect: false,
+				OptionText: "Overridden methods cannot call the superclass version of the method.",
+				IsCorrect:  false,
 			},
 		},
 	}
+
 	body, _ := json.Marshal(payload)
 
 	req, _ := http.NewRequest(http.MethodPost, "http://localhost:8080/service5/mcq/77f7527d-a0e3-4a48-af3e-98f6b85f5fc7", bytes.NewBuffer(body))
-
-	req.Header.Add("Authorization", "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InJhZGFkaXlhdGFwYW45MEBnbWFpbC5jb20iLCJpZCI6Ijk2MjQwN2QxLThhODUtNGFhYi1hYzViLWFkNDQyMmUxNWYzYiIsInJvbGUiOiJBRE1JTiIsIm1mYSI6dHJ1ZSwiaWF0IjoxNzU3ODAyODI3LCJleHAiOjE3NTc4MDM3Mjd9.rHefrgNBLvuLF_DQVZO_LhpQvg1S3kmFds_lAI4pkD4")
-
+	req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", os.Getenv("REFRESH_TOKEN_FOR_TESTCASES")))
+	req.Header.Set("Content-Type", "application/json")
 	data := callApi(req, t, 201)
 
-	var mcqCreateData model.Mcqs
+	var mcqCreateData = &struct {
+		Data model.Mcqs
+	}{}
 
 	if err := json.Unmarshal(data, &mcqCreateData); err != nil {
-		t.Fatalf("Failed to unmarshal response: %v", err)
+		t.Fatalf("Failed to unmarshal response: %+v", err)
 	}
-	tempMCq = mcqCreateData.ID
+	tempMcqId = mcqCreateData.Data.ID
 }
 
-func TestDeleteMCQ(t *testing.T) {
-	fmt.Print("tempMCq", tempMCq)
-	req, _ := http.NewRequest(http.MethodDelete, fmt.Sprintf("http://localhost:8080/service5/mcq/%s", tempMCq), nil)
-	req.Header.Add("Authorization", "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InJhZGFkaXlhdGFwYW45MEBnbWFpbC5jb20iLCJpZCI6Ijk2MjQwN2QxLThhODUtNGFhYi1hYzViLWFkNDQyMmUxNWYzYiIsInJvbGUiOiJBRE1JTiIsIm1mYSI6dHJ1ZSwiaWF0IjoxNzU3ODAyODI3LCJleHAiOjE3NTc4MDM3Mjd9.rHefrgNBLvuLF_DQVZO_LhpQvg1S3kmFds_lAI4pkD4")
+func TestGetMcqData(t *testing.T) {
+	godotenv.Load("../../.env")
+	req, _ := http.NewRequest(http.MethodGet, fmt.Sprintf("http://localhost:8080/service5/mcq/%s", tempMcqId), nil)
+
+	req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", os.Getenv("REFRESH_TOKEN_FOR_TESTCASES")))
 	callApi(req, t, 200)
 }
+
+// Quiz Start
+
+func TestCreateQuiz(t *testing.T) {
+	godotenv.Load("../../.env")
+	courseId, _ := uuid.Parse("77f7527d-a0e3-4a48-af3e-98f6b85f5fc7")
+	var data = &model.Quizes{
+		Title:         "Java Quiz",
+		Description:   "Java Quiz Java QuizJava QuizJava QuizJava QuizJava QuizJava QuizJava Quiz",
+		CourseId:      courseId,
+		TotalQuestion: 1,
+		PassingMarks:  100,
+	}
+	payload, _ := json.Marshal(data)
+
+	req, _ := http.NewRequest(http.MethodPost, "http://localhost:8080/service5/quiz/add", bytes.NewBuffer(payload))
+	req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", os.Getenv("REFRESH_TOKEN_FOR_TESTCASES")))
+	req.Header.Set("Content-Type", "application/json")
+
+	var quizData = &struct {
+		Data model.Quizes
+	}{}
+
+	res := callApi(req, t, 201)
+
+	if err := json.Unmarshal(res, quizData); err != nil {
+		t.Fatalf("Error Unmarshaling Json")
+	}
+	tempQuizId = quizData.Data.ID
+}
+
+func TestDeleteQuiz(t *testing.T) {
+	godotenv.Load("../../.env")
+	req, _ := http.NewRequest(http.MethodDelete, fmt.Sprintf("http://localhost:8080/service5/quiz/%s", tempQuizId), nil)
+
+	req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", os.Getenv("REFRESH_TOKEN_FOR_TESTCASES")))
+	callApi(req, t, 200)
+}
+
+// Quiz End
+
+func TestDeleteMCQ(t *testing.T) {
+	godotenv.Load("../../.env")
+	req, _ := http.NewRequest(http.MethodDelete, fmt.Sprintf("http://localhost:8080/service5/mcq/%s", tempMcqId), nil)
+
+	req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", os.Getenv("REFRESH_TOKEN_FOR_TESTCASES")))
+	callApi(req, t, 200)
+}
+
+// MCQ Related
 
 func callApi(req *http.Request, t *testing.T, expectedStatus int) []byte {
 	client := http.Client{}
@@ -78,12 +126,11 @@ func callApi(req *http.Request, t *testing.T, expectedStatus int) []byte {
 	if err != nil {
 		t.Errorf("Error Exectuting Api")
 	}
-	fmt.Print("res.Body", res.Body)
-	defer res.Body.Close()
+
 	if res.StatusCode != expectedStatus {
 		t.Errorf("Expected Code %d but Got %d", expectedStatus, res.StatusCode)
 	}
-
 	body, _ := io.ReadAll(res.Body)
+	defer res.Body.Close()
 	return body
 }
